3fd66bb205aeb4c0b81e7d7391178029d70cda49
Shraddhabgit <shraddhabodke99@gmail.com>
2026-06-05 17:37:43 +0530

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -51,7 +51,7 @@ sudo mkdir -p /var/www/jerney
 sudo chown -R $USER:$USER /var/www/jerney
 
 # Copy project files (assumes you've transferred them to ~/Jerney)
-cp -r ~/Jerney/* /var/www/jerney/
+cp -r ~/my-3-tier-app-project/* /var/www/jerney/
 
 # --- Install backend dependencies ---
 echo "📦 Installing backend dependencies..."
