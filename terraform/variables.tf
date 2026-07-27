42f460f4f7fd2bfb81ec9bba37f3183af5b432dd
Shraddhabgit <shraddhabodke99@gmail.com>
2026-07-27 17:25:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region" {
+/* variable "aws_region" {
   description = "AWS region to deploy the EKS cluster"
   type        = string
   default     = "us-east-1"
@@ -27,3 +27,4 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+*/
